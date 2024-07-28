b1fb2f5c034bc99a40d3477cc4bf2cc4a718df15
enastaher55 <enastaher06@gmail.com>
2024-07-28 18:30:12 +0300

    disposing notifiers

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -45,6 +45,14 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    isLoadingNotifier.dispose();
+    errorMessageNotifier.dispose();
+    personsNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
